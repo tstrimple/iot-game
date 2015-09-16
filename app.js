a46657da203dd49eaa7f6a3e02186def56ab2912
Timothy Strimple <tim@tstrimple.com>
2015-09-16 13:06:28 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var port = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/move/:x/:y', function(req, res) {
+  var x = req.params.x;
+  var y = req.params.y;
+
+  movePlayer(x, y);
+  res.send('moved');
+});
+
+io.on('connection', function(socket) {
+  console.log('user connected');
+});
+
+function movePlayer(x, y) {
+  io.sockets.emit('move', 1, 1);
+}
+
+http.listen(port, function() {
+  console.log('listening on port', port);
+});
